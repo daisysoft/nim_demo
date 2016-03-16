b103dd25b66802ddc7fb45015f2d6f4503b994fc
zhenlee <zhenlee>
2016-03-17 00:04:55 +0800

    Initial commit

---
+++ demo/build/generated/source/buildConfig/debug/com/netease/nim/demo/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.netease.nim.demo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.netease.nim.demo";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 20;
+  public static final String VERSION_NAME = "";
+  // Fields from default config.
+  public static final String BUILD_DATE = "2016-3-16 22:49:07";
+  public static final String GIT_REVISION = "";
+}
